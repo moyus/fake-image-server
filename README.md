0a0d23fed2b3d84a1f35a8048b35cebaf6df1dcc
liuxinyumichael <liuxinyumichael@didichuxing.com>
2018-08-15 10:31:01 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+<h1 align="center">Fake Image Server</h1>
+
+It's hard to choose suitable image placeholders when you are working on a front-end project. **Fake Image Server** aims to reduce your work by generating clean images with an URL.
+
+## How to use
+Use the `src` attribute to load fake svg images.
+```
+<img src="https://fakeimg.moyu.io/300">
+<img src="https://fakeimg.moyu.io/300/200">
+<img src="https://fakeimg.moyu.io/300/200?bg=red&color=#fff">
+<img src="https://fakeimg.moyu.io/300?text=Hello&size=40">
+```
+
+## Set up your own server
+If you want to set up your own server, make sure follow [node-canvas](https://github.com/Automattic/node-canvas#compiling) instruction to install all the needed system modules.
---
+++ app.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const express = require('express');
+const { createCanvas } = require('canvas');
+const app = express();
+
+app.get('/:width/:height?', main);
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+function main(req, res) {
+  const width = req.params.width ? parseInt(req.params.width) : 200;
+  const height = req.params.height ? parseInt(req.params.height) : width;
+  const bg = req.query.bg || '#cccccc';
+  const color = req.query.color || '#909090';
+  const text = req.query.text || `${width}x${height}`;
+  const fontSize = req.query.size || 30;
+
+  const canvas = createCanvas(width, height, 'svg');
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = bg;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.font = `${fontSize}px Helvetica`;
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'middle';
+  ctx.fillStyle = color;
+
+  ctx.fillText(text, canvas.width / 2, canvas.height / 2);
+
+  const buf = canvas.toBuffer();
+
+  res.set({
+    'Content-Type': 'image/svg+xml',
+    'Access-Control-Allow-Origin': '*'
+  });
+  
+  res.send(buf);
+}
+
+app.listen(8000, function () {
+  console.log('App is listening on port 8000!');
+});
